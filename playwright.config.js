e573ccfb29d8286c21d17bf7fa455639f5fd07eb
ethawn234 <ethawn234@gmail.com>
2025-06-28 10:57:16 -0500

    change playwright reporter from html to list for better testing outputs

--- playwright.config.js
+++ playwright.config.js
@@ -22,7 +22,7 @@ const config = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: `http://127.0.0.1:${process.env.PORT}`,
